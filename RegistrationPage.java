cd186d69c1929948454dd347d07f79c6d69cfce3
udaydurgam <udaydurgam26@gmail.com>
2024-08-13 12:57:59 +0530

    Created and added the files to git

---
+++ RegistrationPage.java
@@ -0,0 +1,61 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class RegistrationPage {
+    WebDriver driver;
+    WebDriverWait wait;
+
+    // Constructor
+    public RegistrationPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, 10);
+    }
+
+    // Web Elements
+    By nameField = By.id("name");
+    By emailField = By.id("email");
+    By passwordField = By.id("password");
+    By mobileField = By.id("mobile");
+    By workStatusDropdown = By.id("labelClass");
+
+    // Actions
+    public void enterName(String name) {
+        driver.findElement(nameField).sendKeys(name);
+    }
+
+    public void enterEmail(String email) {
+        driver.findElement(emailField).sendKeys(email);
+    }
+
+    public void enterPassword(String password) {
+        driver.findElement(passwordField).sendKeys(password);
+    }
+
+    public void enterMobile(String mobile) {
+        driver.findElement(mobileField).sendKeys(mobile);
+    }
+
+    public void setWorkStatus(String workStatus) {
+        try {
+            // Wait for the dropdown to be visible
+            wait.until(ExpectedConditions.visibilityOfElementLocated(workStatusDropdown));
+            WebElement dropdown = driver.findElement(workStatusDropdown);
+            Select select = new Select(dropdown);
+
+            // Select the appropriate option
+            if (workStatus.equalsIgnoreCase("Fresher")) {
+                select.selectByVisibleText("Fresher");
+            } else if (workStatus.equalsIgnoreCase("Experienced")) {
+                select.selectByVisibleText("Experienced");
+            } else {
+                throw new IllegalArgumentException("Invalid work status: " + workStatus);
+            }
+        } catch (Exception e) {
+            System.out.println("Failed to set work status: " + e.getMessage());
+        }
+    }
+}
